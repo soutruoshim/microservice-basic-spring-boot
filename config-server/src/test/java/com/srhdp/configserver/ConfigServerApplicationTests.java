f250ff472e7d1c98ad7e4e278603d2a9936d4959
soutruoshim <sout.rahim@gmail.com>
2023-06-23 07:32:51 +0700

    init config server

---
+++ config-server/src/test/java/com/srhdp/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
